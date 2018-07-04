26d0689aedf446bafdd0cbf7e15163a840d08be5
SaitouRedEyes <zeca.saitou@gmail.com>
2018-07-03 22:52:05 -0300

    Windows Forms ComboBox Example
    This is a simple example of the ComboBox component use.

---
+++ ComboBoxSample/ComboBoxSample/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ComboBoxSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(767, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 306);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.ItemHeight = 13;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Akira",
+            "Cavaleiros do Zodíaco",
+            "Cowboy Bebop",
+            "Dragon Ball",
+            "Inu Yasha",
+            "Sailor Moon",
+            "Sakura Card Captor",
+            "Samurai Warriors",
+            "Samurai X",
+            "Shurato",
+            "Yu Yu Hakusho"});
+            this.comboBox1.Location = new System.Drawing.Point(596, 43);
+            this.comboBox1.MaxDropDownItems = 5;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Animes";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 385);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ComboBoxSample/ComboBoxSample/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch((string)comboBox1.SelectedItem)
+            {
+                case "Akira": pictureBox1.BackgroundImage = Properties.Resources.akira; break;
+                case "Cavaleiros do Zodíaco": pictureBox1.BackgroundImage = Properties.Resources.cavaleiros_do_zodiaco; break;
+                case "Cowboy Bebop": pictureBox1.BackgroundImage = Properties.Resources.cowboy_bebop; break;
+                case "Dragon Ball": pictureBox1.BackgroundImage = Properties.Resources.dragon_ball; break;
+                case "Inu Yasha": pictureBox1.BackgroundImage = Properties.Resources.inu_yasha; break;
+                case "Sailor Moon": pictureBox1.BackgroundImage = Properties.Resources.sailor_moon; break;
+                case "Sakura Card Captor": pictureBox1.BackgroundImage = Properties.Resources.sakura_card_captor; break;
+                case "Samurai Warriors": pictureBox1.BackgroundImage = Properties.Resources.samurai_warriors; break;
+                case "Samurai X": pictureBox1.BackgroundImage = Properties.Resources.samurai_x; break;
+                case "Shurato": pictureBox1.BackgroundImage = Properties.Resources.shurato; break;
+                case "Yu Yu Hakusho": pictureBox1.BackgroundImage = Properties.Resources.yuyu_hakusho; break;
+            }
+        }
+    }
+}
